7dcbcecd99b0f858f7429927d1df1f40cb1328d3
Loboo09 <loboalexander1210@gmail.com>
2026-05-25 21:28:33 -0300

    feat: crear proyectoService con 5 proyectos iniciales

---
+++ src/services/proyectoService.js
@@ -0,0 +1,9 @@
+const proyectoService = (() => {
+  let proyectos = [
+    { id: 1, titulo: "Programación Visual", categoria: "Programación", estado: "En Progreso" },
+    { id: 2, titulo: "Inglés", categoria: "Idiomas", estado: "Pendiente" },
+    { id: 3, titulo: "Programación Estructurada", categoria: "Programación", estado: "Finalizado" },
+    { id: 4, titulo: "Álgebra", categoria: "Matemáticas", estado: "En Progreso" },
+    { id: 5, titulo: "Base de Datos", categoria: "Base de Datos", estado: "Finalizado" }
+  ];
+})();
