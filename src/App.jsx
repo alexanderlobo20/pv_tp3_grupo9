3231817e6f396af736f08ff2e5f25ee802779b55
florencia villar <florenciavillar14@gmail.com>
2026-05-26 12:06:08 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 /* import './App.css'; */
 import './css/style.css';
 import Header from './components/Header';
+import ListaProyectos from './components/ListaProyectos';
 import Footer from './components/Footer';
 
 function App() {
   return (
     <>
       <Header />
+      <ListaProyectos />
       <Footer />
     </>
   );
