9e4d4fb868913bae78ddb0999cbff630a8d13c14
florencia villar <florenciavillar14@gmail.com>
2026-06-05 18:14:42 -0300

    Update ListaProyectos.jsx

--- src/components/ListaProyectos.jsx
+++ src/components/ListaProyectos.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import proyectoService from '../services/proyectoService.js';
 import ProyectoCard from './ProyectoCard';
 import DetalleProyecto from './DetalleProyecto';
+import FormularioProyecto from './FormularioProyecto';
 
 function ListaProyectos() {
   const [proyectos, setProyectos] = useState(
@@ -11,252 +12,22 @@ function ListaProyectos() {
   const [busqueda, setBusqueda] = useState('');
   const [mostrarFormulario, setMostrarFormulario] = useState(false);
   const [proyectoSeleccionado, setProyectoSeleccionado] = useState(null);
-  const [mensaje, setMensaje] = useState('');
-
-  const [nuevoProyecto, setNuevoProyecto] = useState({
-    titulo: '',
-    categoria: '',
-    estado: '',
-    descripcion: '',
-    cantidadIntegrantes: '',
-    integrantes: [],
-    roles: [],
-    recursos: {
-      pdf: '',
-      drive: '',
-      github: '',
-    },
-  });
-
-  const {
-    titulo,
-    categoria,
-    estado,
-    descripcion,
-    cantidadIntegrantes,
-    integrantes,
-    roles,
-    recursos,
-  } = nuevoProyecto;
-
+  
+  const agregarProyecto = (nuevoProyecto) => {
+    proyectoService.agregarProyecto({
+      id: Date.now(),
+      ...nuevoProyecto,
+    });
+  setProyectos(proyectoService.obtenerProyectos());
+  setMostrarFormulario(false);
+  };
+  
   let proyectosFiltrados = proyectos;
-  let formulario = null;
-
   if (busqueda !== '') {
     proyectosFiltrados = proyectoService.buscarProyecto(busqueda);
   }
 
-  const validarProyecto = () => {
-    if (!titulo.trim()) return 'El título es obligatorio';
-    if (!categoria.trim()) return 'La categoría es obligatoria';
-    if (!estado.trim()) return 'El estado es obligatorio';
-    if (!descripcion.trim()) return 'La descripción es obligatoria';
-
-    if (!integrantes.length) return 'Debe haber integrantes';
-
-    if (integrantes.some(i => !i?.trim()))
-      return 'Todos los integrantes deben tener nombre';
-
-    if (roles.some(r => !r?.trim()))
-      return 'Todos los roles deben estar completos';
-
-    return null;
-  };
-
-  if (mostrarFormulario) {
-    formulario = (
-      <div className='formulario'>
-
-        <input
-          type='text'
-          placeholder='Título'
-          value={titulo}
-          onChange={(e) =>
-            setNuevoProyecto({ ...nuevoProyecto, titulo: e.target.value })
-          }
-        />
-
-        <input
-          type='text'
-          placeholder='Categoría'
-          value={categoria}
-          onChange={(e) =>
-            setNuevoProyecto({ ...nuevoProyecto, categoria: e.target.value })
-          }
-        />
-
-        <input
-          type='text'
-          placeholder='Estado'
-          value={estado}
-          onChange={(e) =>
-            setNuevoProyecto({ ...nuevoProyecto, estado: e.target.value })
-          }
-        />
-
-        <input
-          type='text'
-          placeholder='Descripción'
-          value={descripcion}
-          onChange={(e) =>
-            setNuevoProyecto({
-              ...nuevoProyecto,
-              descripcion: e.target.value,
-            })
-          }
-        />
-
-        <input
-          type='number'
-          min='1'
-          max='10'
-          placeholder='Cantidad de integrantes'
-          value={cantidadIntegrantes}
-          onChange={(e) => {
-            let cantidad = Number(e.target.value);
-
-            if (!cantidad || cantidad < 1) cantidad = 1;
-            if (cantidad > 10) cantidad = 10;
-
-            setNuevoProyecto({
-              ...nuevoProyecto,
-              cantidadIntegrantes: cantidad,
-              integrantes: Array(cantidad).fill(''),
-              roles: Array(cantidad).fill(''),
-            });
-          }}
-        />
-
-        {integrantes.map((_, index) => (
-          <input
-            key={index}
-            type='text'
-            placeholder={`Integrante ${index + 1}`}
-            value={integrantes[index] || ''}
-            onChange={(e) => {
-              const nuevos = [...integrantes];
-              nuevos[index] = e.target.value;
-
-              setNuevoProyecto({
-                ...nuevoProyecto,
-                integrantes: nuevos,
-              });
-            }}
-          />
-        ))}
-
-        {roles.map((_, index) => (
-          <input
-            key={index}
-            type='text'
-            placeholder={`Rol del integrante ${index + 1}`}
-            value={roles[index] || ''}
-            onChange={(e) => {
-              const nuevos = [...roles];
-              nuevos[index] = e.target.value;
-
-              setNuevoProyecto({
-                ...nuevoProyecto,
-                roles: nuevos,
-              });
-            }}
-          />
-        ))}
-
-        <input
-          type='text'
-          placeholder='PDF'
-          value={recursos.pdf}
-          onChange={(e) =>
-            setNuevoProyecto({
-              ...nuevoProyecto,
-              recursos: { ...recursos, pdf: e.target.value },
-            })
-          }
-        />
-
-        <input
-          type='text'
-          placeholder='Drive'
-          value={recursos.drive}
-          onChange={(e) =>
-            setNuevoProyecto({
-              ...nuevoProyecto,
-              recursos: { ...recursos, drive: e.target.value },
-            })
-          }
-        />
-
-        <input
-          type='text'
-          placeholder='GitHub'
-          value={recursos.github}
-          onChange={(e) =>
-            setNuevoProyecto({
-              ...nuevoProyecto,
-              recursos: { ...recursos, github: e.target.value },
-            })
-          }
-        />
-
-        {mensaje && <p style={{ color: 'red' }}>{mensaje}</p>}
-
-        <button
-          className='BotonGuardar'
-          onClick={() => {
-            const error = validarProyecto();
-
-            if (error) {
-              setMensaje(error);
-              return;
-            }
-
-            proyectoService.agregarProyecto({
-              id: Date.now(),
-              titulo,
-              categoria,
-              estado,
-              descripcion,
-
-              recursos,
-
-              equipo: integrantes.map((nombre, index) => ({
-                nombre: nombre.trim(),
-                rol: roles[index]?.trim() || '',
-              })),
-            });
-
-            setProyectos(proyectoService.obtenerProyectos());
-
-            setMensaje('Proyecto creado correctamente ✔');
-
-            setTimeout(() => setMensaje(''), 2000);
-
-            setMostrarFormulario(false);
-
-            setNuevoProyecto({
-              titulo: '',
-              categoria: '',
-              estado: '',
-              descripcion: '',
-              cantidadIntegrantes: '',
-              integrantes: [],
-              roles: [],
-              recursos: {
-                pdf: '',
-                drive: '',
-                github: '',
-              },
-            });
-          }}
-        >
-          Guardar
-        </button>
-      </div>
-    );
-  }
-
-  return (
+return (
     <section className='proyectos'>
       <h2 className='proyectos-titulo'>Proyectos</h2>
 
@@ -274,31 +45,42 @@ function ListaProyectos() {
         Agregar Proyecto
       </button>
 
-      {formulario}
+      {mostrarFormulario && (
+        <FormularioProyecto agregarProyecto={agregarProyecto} />
+      )}
 
-      <div className='proyectos-lista'>
-        {proyectosFiltrados.map((proyecto) => (
-          <ProyectoCard
-            key={proyecto.id}
-            proyecto={proyecto}
-            onEliminar={(id) => {
-              proyectoService.eliminarProyecto(id);
-              setProyectos(proyectoService.obtenerProyectos());
-            }}
-            onVerDetalle={(proyecto) => {
-              setProyectoSeleccionado(
-                proyectoSeleccionado?.id === proyecto.id ? null : proyecto
-              );
-            }}
-          />
-        ))}
+      <div className='contenedor-proyectos'>
+        <div className='proyectos-lista'>
+          {proyectosFiltrados.map((proyecto) => (
+            <ProyectoCard
+              key={proyecto.id}
+              proyecto={proyecto}
+              onEliminar={(id) => {
+                proyectoService.eliminarProyecto(id);
+                setProyectos(proyectoService.obtenerProyectos());
+              }}
+              onVerDetalle={(proyecto) => {
+                if (
+                  proyectoSeleccionado &&
+                  proyectoSeleccionado.id === proyecto.id
+                ) {
+                  setProyectoSeleccionado(null);
+                } else {
+                  setProyectoSeleccionado(proyecto);
+                }
+              }}
+            />
+          ))}
+        </div>
       </div>
 
       {proyectoSeleccionado && (
-        <DetalleProyecto
-          proyecto={proyectoSeleccionado}
-          onCerrar={() => setProyectoSeleccionado(null)}
-        />
+        <div className='detalle-contenedor'>
+          <DetalleProyecto
+            proyecto={proyectoSeleccionado}
+            onCerrar={() => setProyectoSeleccionado(null)}
+          />
+        </div>
       )}
     </section>
   );
