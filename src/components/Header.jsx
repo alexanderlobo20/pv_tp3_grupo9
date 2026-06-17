a99b928fcc8110595b2549dea38d5c9732fd7fca
fernando-eb2406 <feb-332@hotmail.com>
2026-06-17 18:40:41 -0300

    feat: conectar header al estado global

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,29 @@
+import { useContext } from 'react';
 import Nav from './Nav';
+import { UsuarioContext } from '../context/UsuarioContext';
 
 function Header() {
+  const { usuario } = useContext(UsuarioContext);
+
   return (
     <>
       <header>
         <Nav />
+        <div>
+          Usuario: {usuario.nombre}
+          <br />
+          Rol: {usuario.rol}
+        </div>
       </header>
-      <div className='welcome'>
-        <h1>Plataforma de Gestión de Proyectos Educativos</h1>
-        <p>
-          Administra, supervisa y mejora tus proyectos educativos de forma
-          estructurada y organizada.
-        </p>
-      </div>
+      {
+        <div className='welcome'>
+          <h1>Plataforma de Gestión de Proyectos Educativos</h1>
+          <p>
+            Administra, supervisa y mejora tus proyectos educativos de forma
+            estructurada y organizada.
+          </p>
+        </div>
+      }
     </>
   );
 }
